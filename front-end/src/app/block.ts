87a7eefe08121e5368b879fdb2edfdc8473868fd
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-05 21:30:29 +0300

    create block and entry class

---
+++ front-end/src/app/block.ts
@@ -0,0 +1,22 @@
+
+
+import { Entry } from './entry';
+
+export class Block {
+  _id: string;
+  title: string;
+  slug: string;
+  data: Array<any>;
+
+  constructor(
+    _id?: string,
+    title?: string,
+    slug?: string,
+    data?: Array<any>
+  ) {
+    this._id = _id;
+    this.title = title;
+    this.slug = slug;
+    this.data = data || [new Entry()];
+  }
+}
