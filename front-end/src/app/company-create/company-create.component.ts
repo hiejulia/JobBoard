a337d55a2ead1bb09b0b689ebd40d150509fc2bb
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-05 19:56:43 +0300

    init company component

--- front-end/src/app/company-create/company-create.component.ts
+++ front-end/src/app/company-create/company-create.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, RouterLink } from '@angular/router';
+import { CompanyService } from '../company.service';
+// import { Company } from '../company.model';
+
 @Component({
   selector: 'app-company-create',
   templateUrl: './company-create.component.html',
   styleUrls: ['./company-create.component.css']
 })
 export class CompanyCreateComponent implements OnInit {
+  public company: Company;
+  private _router: Router;
+  private _companyService: CompanyService;
 
-  constructor() { }
+  constructor(companyService: CompanyService, router: Router) {
+    this._router = router;
+    this._companyService = companyService;
+
+   }
 
   ngOnInit() {
+    this.company = new Company();
   }
 
 }
