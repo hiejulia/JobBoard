60aff4e77c76bf433130de5caeaa562f48cde9de
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-05 20:14:33 +0300

    create jobs component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { CompanyCreateComponent } from './company-create/company-create.component';
 import { CompanyListComponent } from './company-list/company-list.component';
 import { JobBaseComponent } from './job-base/job-base.component';
+import { JobsComponent } from './jobs/jobs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyCreateComponent,
     CompanyListComponent,
-    JobBaseComponent
+    JobBaseComponent,
+    JobsComponent
   ],
   imports: [
     BrowserModule,
--- front-end/src/app/job-base/job-base.component.ts
+++ front-end/src/app/job-base/job-base.component.ts
@@ -5,6 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './job-base.component.html',
   styleUrls: ['./job-base.component.css']
 })
+
+
+
+// @RouteConfig([
+//   { path: '/', as: 'JobList', component: JobListComponent, useAsDefault: true },
+//   { path: '/:id/:slug', as: 'JobDetail', component: JobDetailComponent },
+//   { path: '/create', as: 'JobCreate', component: JobCreateComponent }
+// ])
+
+
+
 export class JobBaseComponent implements OnInit {
 
   constructor() { }
