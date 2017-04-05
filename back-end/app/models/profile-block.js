8e57a0761e41bc8729c3dc4d213802c5ca490d57
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-05 21:15:46 +0300

    finish profile-block schema

--- back-end/app/models/profile-block.js
+++ back-end/app/models/profile-block.js
@@ -3,3 +3,19 @@
 const mongoose = require('mongoose');
 const commonHelper = require('../helpers/common');
 const Schema = mongoose.Schema;
+
+let ProfileBlock = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  slug: String,
+  data: []
+});
+
+ProfileBlock.pre('save', function(next) {
+  this.slug = commonHelper.createSlug(this.title);
+  next();
+});
+
+module.exports = mongoose.model('ProfileBlock', ProfileBlock);
