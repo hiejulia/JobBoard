4e5775bba776e1d6ea5e745de5ca9568da8a89ae
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-05 20:04:09 +0300

    finish company list component

--- front-end/src/app/company-list/company-list.component.ts
+++ front-end/src/app/company-list/company-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { Router, RouterLink } from '@angular/router';
+import { CompanyService } from '../company.service';
+// import { Company } from '../company.model';
+
 @Component({
   selector: 'app-company-list',
   templateUrl: './company-list.component.html',
   styleUrls: ['./company-list.component.css']
 })
 export class CompanyListComponent implements OnInit {
+  public companies: Array<Company>;
+  private _router: Router;
+  private _companyService: CompanyService;
 
-  constructor() { }
+  constructor(companyService: CompanyService, router: Router) {
+    this._router = router;
+    this._companyService = companyService;
+   }
 
   ngOnInit() {
+    this._companyService
+      .getAll().subscribe((companies) => {
+        this.companies = companies;
+      });
+
   }
 
 }
