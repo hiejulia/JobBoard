659b34b4f80b84fbb12efa485a57681e70cb44d3
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-05 21:00:06 +0300

    finish company detail

--- front-end/src/app/company-detail/company-detail.component.ts
+++ front-end/src/app/company-detail/company-detail.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { Params, RouterLink } from '@angular/router';
+import { CompanyService } from '../company.service';
+import { Company } from '../company.model';
+import { JobsComponent } from '../../job/index';
+
 @Component({
   selector: 'app-company-detail',
   templateUrl: './company-detail.component.html',
   styleUrls: ['./company-detail.component.css']
 })
 export class CompanyDetailComponent implements OnInit {
+  public company: Company;
+  private _routeParams: RouteParams;
+  private _companyService: CompanyService;
 
-  constructor() { }
+  constructor(companyService: CompanyService, routerParams: Params) {
+    this._routeParams = routerParams;
+    this._companyService = companyService;
+   }
 
   ngOnInit() {
+    const id: string = this._routeParams.get('id');
+    this.company = new Company();
+    this._companyService
+    .findById(id)
+    .subscribe((company) => {
+      this.company = company;
+    });
   }
 
 }
