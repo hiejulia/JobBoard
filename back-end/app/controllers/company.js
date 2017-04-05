269ea7ba32f916b9453502264ed25f94d22c4d87
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-05 17:22:46 +0300

    updating the company controller for backend

--- back-end/app/controllers/company.js
+++ back-end/app/controllers/company.js
@@ -45,12 +45,58 @@ function findCompanyById(req,res, next){
 }
 /**GET ALL THE COMPANY  */
 function getAllCompanies(req,res, next){
-    
+    const limit = + req.query.limit || 50;
+    const skip = +req.query.skip || 0;
+    let query = _.pick(req.query,['country']);
+    Company
+  .find(query)
+  .limit(limit)
+  .skip(skip)
+  .exec((err, companies) => {
+    if (err) {
+      return next(err);
+    }
+
+    req.resources.companies = companies;
+    next();
+  });
+}
+//UPDATE COMPANY 
+function updateCompany(req, res, next){
+    let data = _.pick(req.body, ['name', 'country', 'address']);
+    _.assign(req.resources.company,req.body);
+    req.resources.company.save((err,updatedCompany) => {
+        if(err){
+            return next(err);
+        }
+        req.resources.company = updatedCompany;
+        next();
+    })
+
+}
+//ADD A COMPANY MEMBER 
+function addCompanyMember(req, res, next) {
+    let includes = _.includes(req.resources.company.members, req.body.member);
+    if(includes){
+        return res.status(409).json({message:'User is already a member of your company',type: 'already_member'});
+    }
+    req.resources.company.members.push(req.body.user);
+    req.resources.company.save((err, updatedCompany) => {
+    if (err) {
+      return next(err);
+    }
+
+    req.resources.company = updatedCompany;
+    next();
+  });
+
+
 }
 
+//REMOVE COMPANY MEMBER 
+function removeCompanyMember(req, res, next) {
 
-
-
+}
 
 
 
@@ -60,3 +106,6 @@ function getAllCompanies(req,res, next){
 module.exports.create = createCompany;
 module.exports.findById = findCompanyById;
 module.exports.getAll = getAllCompanies;
+module.exports.update = updateCompany;
+module.exports.addMember = addCompanyMember;
+module.exports.removeMember = removeCompanyMember;
