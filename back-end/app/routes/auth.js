58975c114b12091950c0c7ac4efcd64a7634500f
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-06 23:30:22 +0300

    commit all

--- back-end/app/routes/auth.js
+++ back-end/app/routes/auth.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/authentication');
+const authCtrl = require('../controllers/authentication');
+const userCtrl = require('../controllers/user');
+const accountCtrl = require('../controllers/account');
+
+
+
+router.post('/login', authCtrl.signin);
+router.get('/signout', authCtrl.signout);
+
+router.post('/register', accountCtrl.register);
+//make user the user is authenticated when get info route 
+router.get('/info', auth.ensured, userCtrl.getAuthUser);
+
+module.exports = router;
