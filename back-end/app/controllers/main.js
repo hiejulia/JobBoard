1c558719b1df2c5480aec7e9bfb08a14a9f05fcf
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-07 23:27:03 +0300

    updating the controller in back end

---
+++ back-end/app/controllers/main.js
@@ -0,0 +1,21 @@
+'use strict';
+//show page function
+function showPage(page,data){
+  return function(req,res,next){
+    res.locals.historyData = req.session.historyData || {};
+    res.render(page,data);
+    delete req.session.historyData;
+  }
+}
+
+function responseToJSON(prop){
+  return function(req,res, next){
+    res.json(req.resources[prop]);
+  }
+}
+
+
+
+module.exports.showPage = showPage;
+module.exports.toJSON = responseToJSON;
+
