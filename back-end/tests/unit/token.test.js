25f555717524003ac242384b09402cbf780ecd3d
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-07 16:32:25 +0300

    create test for token helper and password helper

---
+++ back-end/tests/unit/token.test.js
@@ -0,0 +1,25 @@
+'use strict';
+var chai = require('chai');
+var should = chai.should();
+var tokenHelper = require('../../app/helpers/token');
+
+describe('Token Helper', function() {
+  describe('#generate() - generate token', function() {
+       it('should generate a random 16 length token', function(done) {
+           tokenHelper.generate(function(err, token){
+               if(err) throw err;
+               should.exist(token);
+               token.should.be.a('string');
+               token.length.should.equal(16);
+                done();
+           });
+
+       });
+
+
+  });
+
+
+
+
+});
