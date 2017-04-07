383540e42b8db204c277bc214797b35261eb00f7
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-07 21:38:23 +0300

    passport in the back end

---
+++ back-end/config/passport.js
@@ -0,0 +1,22 @@
+'use strict';
+const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');//model User
+
+function initPassport(app){
+    passport.serializeUser((user,done) => {
+        done(null,user.id);
+    });
+
+    passport.deserializeUser((user,done) => {
+        User.findById(id,done);
+
+    });
+
+    //load strategies
+    require('./strategies/local').init();
+}
+
+
+
+module.exports.init = initPassport;
