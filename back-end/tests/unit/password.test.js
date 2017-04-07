e0061719554f56e740a1bafc31bb474d9c1bfb49
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-07 10:45:12 +0300

    create unit test

---
+++ back-end/tests/unit/password.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var chai = require('chai');
+var should = chai.should();
+var passwordHelper = require('../../app/helpers/password');
+
+describe('This is password helper ', function() {
+
+
+});
